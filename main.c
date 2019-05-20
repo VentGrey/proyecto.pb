a3b04fed6a8f6c7a45accd56ab0a04cc8c40d8f1
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-20 02:12:43 -0500

    Added headers

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
+/* Bibliotecas */
+
+// Standard Input Output //
 #include <stdio.h>
+
+// Standard Library //
+#include <stdlib.h>
+
+// Dynamic memory management //
+#include <malloc.h>
+
+// Booleans library (Requires C99 +) //
+#include <stdbool.h>
+
+// String management library //
+#include <string.h>
+
+/* Estructuras */
+
+// Estructura del alojamiento
+
+typedef struct alojamiento {
+    int id;
+    char
+}
