c7fa2f3801229c9edc0463e6cfd4ca10a45ce00c
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-27 01:50:22 -0500

    refactor file

--- main.c
+++ main.c
@@ -1,130 +1,349 @@
-/* Bibliotecas */
-
-// Standard Input Output //
 #include <stdio.h>
-
-// Standard Library //
 #include <stdlib.h>
 
-// Dynamic memory management //
-#include <malloc.h>
-
-// Booleans library (Requires C99 +) //
-#include <stdbool.h>
-
-// String management library //
-#include <string.h>
-
-/* Estructuras */
-
-// Estructura del stay
-typedef struct stay {
-        int id;
-        char longitud[20];
-        char latitud[20];
-        char tipo[50];
-        char fecha[50];
-} stay;
-
-// Estructura de histoŕico
-typedef struct historial {
-        int id;
-        float precio;
-        int calif;
-        char fechas_disp[100];
-} historial;
-
-// Archivo a guardar
-FILE *file = NULL;
-
-/* Prototipos de funciones */
-
-//-- Archivos
-void crear_archivo_aloj();
-void cerrar_archivo_aloj();
-
-void crear_archivo_hist();
-void cerrar_archivo_hist();
-
-//-- Interfaces
 void menu();
-void ejecutar(char tecla);
 
+typedef struct{
+    int codigo;
+    char detalle[30];
+    float precio;
+    float costo;
+    char prov[30];
+}Producto;
 
-//-- Godínez
-void insertar_aloj();
-void mostrar_aloj();
+typedef struct{
+    int codigo;
+    char nombre[30];
+    char telefono[30];
+    char email[30];
+}Proveedores;
 
-void insertar_hist();
-void mostrar_hist();
+typedef struct{
+    char cuit[30];
+    char cliente[30];
+    char fechaalta[11];
+    char email[30];
+    char telefono[30];
+}Clientes;
 
-/* MAIN */
-int main(int argc, char *argv[])
-{
+typedef struct{
+    int codigo;
+    char fecha[11];
+    char cuit[30];
+    float total;
+    float descuento;
+}Presupuesto;
 
-        return 0;
+typedef struct{
+    int codigo;
+    char producto[30];
+    int cantidad;
+    float precio;
+}DetPresupuesto;
+
+void AltaPresu();
+void BajaPresu();
+void ModifPresu();
+void ListadoPresu();
+void AltaProds();
+void BajaProds();
+void ModifProds();
+void ListadoProds();
+void AltaProvs();
+void BajaProvs();
+void ModifProvs();
+void ListadoProvs();
+void AltaClientes();
+void BajaClientes();
+void ModifClientes();
+void ListadoClientes();
+
+int main(){
+    menu();
+    return 0;
 }
 
-/* FUNCIONES */
-void crear_archivo_aloj()
-{
-        file = fopen("info.hsp", "w+b");
-        int tamanio = sizeof(struct stay);
+void menu(){
+    int opcion,opcion2;
+    do{
+        printf("             -------------------------------------------\n");
+        printf("             -------------------------------------------\n");
+        printf("                          1) Presupuestos\n");
+        printf("                          2) Productos\n");
+        printf("                          3) Proveedores\n");
+        printf("                          4) Clientes\n");
+        printf("                          5) Salir\n");
+        printf("             -------------------------------------------\n");
+        printf("             -------------------------------------------\n");
+        scanf("%d",&opcion);
+        system("clear");
+        printf("             -------------------------------------------\n");
+        printf("             -------------------------------------------\n");
+        printf("                          1) Alta\n");
+        printf("                          2) Baja\n");
+        printf("                          3) Modificación\n");
+        printf("                          4) Listado\n");
+        printf("                          5) Volver al Menú\n");
+        printf("             -------------------------------------------\n");
+        printf("             -------------------------------------------\n");
+        scanf("%d",&opcion2);
+        system("clear");
+            switch (opcion){
+                case 1:
+                        switch(opcion2){
+                            case 1:
+                                AltaPresu();
+                                break;
+                            case 2:
+                                BajaPresu();
+                                break;
+                            case 3:
+                                ModifPresu();
+                                break;
+                            case 4:
+                                ListadoPresu();
+                                break;
+                            case 5:
+                                menu();
+                                break;
+                        }
+                    break;
+                case 2:
+                        switch(opcion2){
+                            case 1:
+                                AltaProds();
+                                break;
+                            case 2:
+                                BajaProds();
+                                break;
+                            case 3:
+                                ModifProds();
+                                break;
+                            case 4:
+                                ListadoProds();
+                                break;
+                            case 5:
+                                menu();
+                                break;
+                        }
+                    break;
+                case 3:
+                        switch(opcion2){
+                            case 1:
+                                //AltaProvs();
+                                break;
+                            case 2:
+                                //BajaProvs();
+                                break;
+                            case 3:
+                                //ModifProvs();
+                                break;
+                            case 4:
+                                //ListadoProvs();
+                                break;
+                            case 5:
+                                menu();
+                                break;
+                        }
+                    break;
+                case 4:
+                        switch(opcion2){
+                            case 1:
+                                //AltaClientes();
+                                break;
+                            case 2:
+                                //BajaClientes();
+                                break;
+                            case 3:
+                                //ModifClientes();
+                                break;
+                            case 4:
+                                //ListadoClientes();
+                                break;
+                            case 5:
+                                menu();
+                                break;
+                        }
+                    break;
+                case 5:
+                    break;
 
-        FILE *main_file = fopen("datos.hsp", "r+b");
-
-        // Revisar si el archivo existe
-        if (main_file != NULL) {
-                struct stay * entidad = (struct stay *) malloc(tamanio);
-                fseek(main_file, 0 , SEEK_SET);
-
-                do {
-                        fread(entidad, tamanio, 1, main_file);
-                        fwrite(entidad, tamanio, 1, file);
-                } while (!feof(main_file));
-
-                fclose(main_file);
-        }
+            }
+    }while (opcion!=5);
 }
 
-void cerrar_archivo_aloj() {
-        FILE *main_file = fopen("datos.hsp", "w+b");
-        struct stay * entidad = (struct stay *) malloc(sizeof(struct stay));
-
-        fseek(file, 0, SEEK_SET);
-
-        do {
-                fread(entidad, sizeof(struct stay), 1, file);
-                fwrite(entidad, sizeof(struct stay), 1, main_file);
-        } while (!feof(file));
-
-        fclose(main_file);
-        fclose(file);
+void AltaPresu(){
+    FILE *pf;
+    Presupuesto presu;
+    pf = fopen("Presupuesto.dat","ab");
+    printf("Ingrese Código\n");
+    scanf("%i",&presu.codigo);
+    printf("Ingrese Fecha\n");
+    scanf("%s",presu.fecha);
+    printf("Ingrese CUIT\n");
+    scanf("%s",presu.cuit);
+    printf("Ingrese Total\n");
+    scanf("%f",&presu.total);
+    printf("Ingrese Descuento\n");
+    scanf("%f",&presu.descuento);
+    fseek(pf,0L,SEEK_END);
+    fwrite(&presu,sizeof(Presupuesto),1,pf);
+    fclose(pf);
+    system("clear");
+    menu();
 }
 
-void menu() {
-        char op;
-        while (op != 'S' && op != 's') {
-                printf("---- ALOJAMIENTOS -----\n");
-                printf("1 - Ingresar un alojamiento\n");
-                printf("2 - Mostrar un alojamiento\n");
-                printf("3 - Eliminar un alojamiento\n");
-                printf("4 - Mostrar todos los alojamientos registrados\n");
-                printf("---- HISTORIAL -------\n");
-                printf("5 - Ingresar un elemento en el historial\n");
-                printf("6 - Mostrar un elemento del historial\n");
-                printf("7 - Eliminar un elemento del historial");
-                printf("8 - Mostrar todos los elementos del historial");
-                scanf("%c", op);
+void ListadoPresu(){
+    FILE *pf;
+    Presupuesto presu;
+    pf = fopen("Presupuesto.dat","rb");
+    fread(&presu,sizeof(Presupuesto),1,pf);
+    while(!feof(pf)){
+        printf("%i ; %s ; %s ; %.2f ; %.2f\n",presu.codigo,presu.fecha,presu.cuit,presu.total,presu.descuento);
+        fread(&presu,sizeof(Presupuesto),1,pf);
+    }
+    fclose(pf);
+}
 
-                if (op != 'S' && op != 's') {
-                        ejecutar(op);
+void ModifPresu(){
+    FILE *pf,*pfaux;
+    Presupuesto presu;
+    int codigoaux;
+    pf = fopen("Presupuesto.dat","rb");
+    pfaux = fopen("Presupuestoaux.dat","ab");
+    printf("Ingrese Código\n");
+    scanf("%i",&codigoaux);
+    fread(&presu,sizeof(Presupuesto),1,pf);
+        while (!feof(pf)){
+                if (presu.codigo != codigoaux){
+                    fseek(pfaux,0l,SEEK_END);
+                    fwrite(&presu,sizeof(Presupuesto),1,pfaux);
+                }else{
+                    printf("Ingrese Fecha\n");
+                    scanf("%s",presu.fecha);
+                    printf("Ingrese CUIT\n");
+                    scanf("%s",presu.cuit);
+                    printf("Ingrese Total\n");
+                    scanf("%f",&presu.total);
+                    printf("Ingrese Descuento\n");
+                    scanf("%f",&presu.descuento);
+                    fseek(pfaux,0l,SEEK_END);
+                    fwrite(&presu,sizeof(Presupuesto),1,pfaux);
                 }
+            fread(&presu,sizeof(Presupuesto),1,pf);
         }
+    fclose(pf);
+    fclose(pfaux);
+    remove("Presupuesto.dat");
+    rename("Presupuestoaux.dat","Presupuesto.dat");
 }
 
-void ejecutar(char tecla) {
-        if (tecla == '1') {
-
+void BajaPresu(){
+    FILE *pf,*pfaux;
+    Presupuesto presu;
+    int codigoaux;
+    pf = fopen("Presupuesto.dat","rb");
+    pfaux = fopen("Presupuestoaux.dat","ab");
+    printf("Ingrese Código\n");
+    scanf("%i",&codigoaux);
+    fread(&presu,sizeof(Presupuesto),1,pf);
+        while (!feof(pf)){
+                if (presu.codigo != codigoaux){
+                    fseek(pfaux,0l,SEEK_END);
+                    fwrite(&presu,sizeof(Presupuesto),1,pfaux);
+                }
+            fread(&presu,sizeof(Presupuesto),1,pf);
         }
+    fclose(pf);
+    fclose(pfaux);
+    remove("Presupuesto.dat");
+    rename("Presupuestoaux.dat","Presupuesto.dat");
+}
+
+void AltaProds(){
+    FILE *pf;
+    Producto prod;
+    pf = fopen("Productos.dat","ab");
+    printf("Ingrese Codigo\n");
+    scanf("%i",&prod.codigo);
+    printf("Ingrese Detalle\n");
+    scanf("%s",prod.detalle);
+    printf("Ingrese Precio\n");
+    scanf("%f",&prod.precio);
+    printf("Ingrese Costo\n");
+    scanf("%f",&prod.costo);
+    printf("Ingrese Proveedor\n");
+    scanf("%s",prod.prov);
+    fseek(pf,0L,SEEK_END);
+    fwrite(&prod,sizeof(Producto),1,pf);
+    fclose(pf);
+    system("clear");
+    menu();
+}
+
+void ListadoProds(){
+    FILE *pf;
+    Producto prod;
+    pf = fopen("Productos.dat","rb");
+    fread(&prod,sizeof(Producto),1,pf);
+    while(!feof(pf)){
+        printf("%i ; %s ; %.2f ; %.2f ; %s\n",prod.codigo,prod.detalle,prod.precio,prod.costo,prod.prov);
+        fread(&prod,sizeof(Producto),1,pf);
+    }
+    fclose(pf);
+}
+
+void ModifProds(){
+    FILE *pf,*pfaux;
+    Producto prod;
+    int codigoaux;
+    pf = fopen("Productos.dat","rb");
+    pfaux = fopen("Productosaux.dat","ab");
+    printf("Ingrese Código\n");
+    scanf("%i",&codigoaux);
+    fread(&prod,sizeof(Producto),1,pf);
+        while (!feof(pf)){
+                if (prod.codigo != codigoaux){
+                    fseek(pfaux,0l,SEEK_END);
+                    fwrite(&prod,sizeof(Producto),1,pfaux);
+                }else{
+                    printf("Ingrese Detalle\n");
+                    scanf("%s",prod.detalle);
+                    printf("Ingrese Precio\n");
+                    scanf("%f",&prod.precio);
+                    printf("Ingrese Costo\n");
+                    scanf("%f",&prod.costo);
+                    printf("Ingrese Proveedor\n");
+                    scanf("%s",prod.prov);
+                    fseek(pfaux,0l,SEEK_END);
+                    fwrite(&prod,sizeof(Producto),1,pfaux);
+                }
+            fread(&prod,sizeof(Producto),1,pf);
+        }
+    fclose(pf);
+    fclose(pfaux);
+    remove("Productos.dat");
+    rename("Productosaux.dat","Productos.dat");
+}
+
+void BajaProds(){
+    FILE *pf,*pfaux;
+    Producto prod;
+    int codigoaux;
+    pf = fopen("Productos.dat","rb");
+    pfaux = fopen("Productosaux.dat","ab");
+    printf("Ingrese Código\n");
+    scanf("%i",&codigoaux);
+    fread(&prod,sizeof(Producto),1,pf);
+        while (!feof(pf)){
+                if (prod.codigo != codigoaux){
+                    fseek(pfaux,0l,SEEK_END);
+                    fwrite(&prod,sizeof(Producto),1,pfaux);
+                }
+            fread(&prod,sizeof(Producto),1,pf);
+        }
+    fclose(pf);
+    fclose(pfaux);
+    remove("Productos.dat");
+    rename("Productosaux.dat","Productos.dat");
 }
